84f65599e9659f4f8fd43bb2925ca76f4470114e
Miron <rybak.m.yu@gmail.com>
2023-11-24 17:18:36 +0300

    GameActivity + ResultActivity
    GameActivity:
    added logic to buttons, now counter increments and quiz resets every press.
    ResultActivity:
    made layout + logic to return button

--- app/src/main/java/com/bano/finquiz/GameActivity.java
+++ app/src/main/java/com/bano/finquiz/GameActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity{
+
+    public static int count;
+    public static String answer;
     private Thread thread;
     TextView termText;
     Button def1Button;
     Button def2Button;
     Button def3Button;
     TextView timeText;
+    TextView counterText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,13 +29,58 @@ public class GameActivity extends AppCompatActivity {
 
         termText = findViewById(R.id.term);
         timeText = findViewById(R.id.time);
+        counterText = findViewById(R.id.count);
+
         def1Button = findViewById(R.id.definition1);
         def2Button = findViewById(R.id.definition2);
         def3Button = findViewById(R.id.definition3);
 
-        generateQuize();
+        answer = generateQuize();
 
         startCountdown();
+
+        def1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(def1Button.getText() == answer) {
+                    count += 1;
+                    counterText.setText("Счет: " + count);
+                    answer = generateQuize();
+                }
+                else {
+                    Toast.makeText(GameActivity.this,"Неверно", Toast.LENGTH_SHORT).show();
+                    answer = generateQuize();
+                }
+            }
+        });
+        def2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(def2Button.getText() == answer) {
+                    count += 1;
+                    counterText.setText("Счет: " + count);
+                    answer = generateQuize();
+                }
+                else {
+                    Toast.makeText(GameActivity.this,"Неверно", Toast.LENGTH_SHORT).show();
+                    answer = generateQuize();
+                }
+            }
+        });
+        def3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(def3Button.getText() == answer) {
+                    count += 1;
+                    counterText.setText("Счет: " + count);
+                    answer = generateQuize();
+                }
+                else {
+                    Toast.makeText(GameActivity.this,"Неверно", Toast.LENGTH_SHORT).show();
+                    answer = generateQuize();
+                }
+            }
+        });
 
     }
 
@@ -44,7 +94,7 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    public void startCountdown(){
+    public void startCountdown(){          // начало отсчета 30 сек
         thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -71,36 +121,66 @@ public class GameActivity extends AppCompatActivity {
         thread.start();
     }
 
-    public void generateQuize(){
+    public String generateQuize(){
         String[] terms = getResources().getStringArray(R.array.terms_for_game);
         String[] defs = getResources().getStringArray(R.array.def_for_game);
 
         Random random = new Random();
-        int randomTermIndex = random.nextInt(terms.length);
+        int randomTermIndex = random.nextInt(terms.length);  // получение случайного индекса термина
 
         termText.setText(terms[randomTermIndex]);
 
         int numberOfUsedButton;
-        int randomButtonIndex = random.nextInt(3);
+        String buttonInside;
+        int randomButtonIndex = random.nextInt(3);    // установка правильного ответа в случаную кнопку
         switch (randomButtonIndex){
             case 0:{
                 def1Button.setText(defs[randomTermIndex]);
                 numberOfUsedButton = 1;
+                buttonInside = defs[randomTermIndex];
                 break;
             }
             case 1:{
                 def2Button.setText(defs[randomTermIndex]);
                 numberOfUsedButton = 2;
+                buttonInside = defs[randomTermIndex];
                 break;
             }
             case 2:{
                 def3Button.setText(defs[randomTermIndex]);
                 numberOfUsedButton = 3;
+                buttonInside = defs[randomTermIndex];
                 break;
             }
+            default:
+                throw new IllegalStateException("Unexpected value: " + randomButtonIndex);
         }
+
+        for(int j = 1; j <= 3; j++){                        // установка остальных кнопок
+            if(j == numberOfUsedButton) continue;
+            switch (j){
+                case 1:{
+                    int randomDefIndex = random.nextInt(defs.length);
+                    while (randomDefIndex == randomTermIndex) randomDefIndex = random.nextInt(defs.length);
+                    def1Button.setText(defs[randomDefIndex]);
+                    break;
+                }
+                case 2:{
+                    int randomDefIndex = random.nextInt(defs.length);
+                    while (randomDefIndex == randomTermIndex) randomDefIndex = random.nextInt(defs.length);
+                    def2Button.setText(defs[randomDefIndex]);
+                    break;
+                }
+                case 3:{
+                    int randomDefIndex = random.nextInt(defs.length);
+                    while (randomDefIndex == randomTermIndex) randomDefIndex = random.nextInt(defs.length);
+                    def3Button.setText(defs[randomDefIndex]);
+                    break;
+                }
+            }
+
+        }
+        return buttonInside;
     }
-
-
 
 }
--- app/src/main/java/com/bano/finquiz/ResultActivity.java
+++ app/src/main/java/com/bano/finquiz/ResultActivity.java
@@ -2,13 +2,35 @@ package com.bano.finquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView record;
+    TextView count;
+    TextView wins;
+    TextView warning;
+    Button returnBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        record = findViewById(R.id.recordText);
+        count = findViewById(R.id.countText);
+        wins = findViewById(R.id.winsText);
+        warning = findViewById(R.id.warningText);
+        returnBut = findViewById(R.id.returnButton);
+
+
+    }
+
+    public void  onClickReturn(View view) {
+        startActivity(new Intent(ResultActivity.this, MainActivity.class));
     }
 }
